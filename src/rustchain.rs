558aff90f8f896b8909e8c5b9d07dbbf591e6ad4
dhruvji <dhruvgautam@berkeley.edu>
2023-05-15 00:07:28 -0700

    initializing

--- src/rustchain.rs
+++ src/rustchain.rs
@@ -3,3 +3,170 @@ use sha2::{Sha256, Digest};
 use std::fmt::Write;
 use chrono::prelude::*;
 
+#[derive(Debug, Clone, Serialize)]
+struct Transact {
+    sender: String,
+    receiver: String,
+    amount: f32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct bheader {
+    timestamp: i64,
+    nonce: u32,
+    pre_hash: String,
+    merkle: String,
+    difficulty: u32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct block {
+    header: bheader,
+    count: u32,
+    transactions: Vec<Transact>,
+}
+
+pub struct Chain {
+    chain: Vec<block>,
+    curr_trans: Vec<Transact>,
+    difficulty: u32,
+    miner_addr: String,
+    reward: f32,
+}
+
+impl Chain {
+    pub fn new(miner_addr: String, difficulty: u32) -> Chain {
+        let mut chain = Chain {
+            chain: Vec::new(),
+            curr_trans: Vec::new(),
+            difficulty,
+            miner_addr,
+            reward: 100.0,
+        };
+
+        chain.generate_new_block();
+        chain
+    }
+
+    pub fn new_transaction(&mut self, sender: String, receiver: String, amount: f32) -> bool {
+        self.curr_trans.push(Transact {
+            sender,
+            receiver,
+            amount,
+        });
+
+        true
+    }
+
+    pub fn last_hash(&self) -> String {
+        let block = match self.chain.last() {
+            Some(block) => block,
+            None => return String::from_utf8(vec![48; 64]).unwrap(),
+        };
+        Chain::hash(&block.header)
+    }
+
+    pub fn update_difficulty(&mut self, difficulty: u32) -> bool {
+        self.difficulty = difficulty;
+        true
+    }
+
+    pub fn update_reward(&mut self, reward: f32) -> bool {
+        self.reward = reward;
+        true
+    }
+
+    pub fn generate_new_block(&mut self) -> bool {
+        let header = bheader {
+            timestamp: Utc::now().timestamp_millis(),
+            nonce: 0,
+            pre_hash: self.last_hash(),
+            merkle: String::new(),
+            difficulty: self.difficulty,
+        };
+
+        let reward_trans = Transact {
+            sender: String::from("Root"),
+            receiver: self.miner_addr.clone(),
+            amount: self.reward,
+        };
+
+        let mut block = block {
+            header,
+            count: 0,
+            transactions: vec![],
+        };
+
+        block.transactions.push(reward_trans);
+        block.transactions.append(&mut self.curr_trans);
+        block.count = block.transactions.len() as u32;
+        block.header.merkle = Chain::get_merkle(block.transactions.clone());
+        Chain::proof_of_work(&mut block.header);
+
+        println!("{:#?}", &block);
+        self.chain.push(block);
+        true
+    }
+
+    fn get_merkle(curr_trans: Vec<Transact>) -> String {
+        let mut merkle = Vec::new();
+
+        for t in &curr_trans {
+            let hash = Chain::hash(t);
+            merkle.push(hash);
+        }
+
+        if merkle.len() % 2 == 1 {
+            let last = merkle.last().cloned().unwrap();
+            merkle.push(last);
+        }
+
+        while merkle.len() > 1 {
+            let mut h1 = merkle.remove(0);
+            let mut h2 = merkle.remove(0);
+            h1.push_str(&mut h2);
+            let nh = Chain::hash(&h1);
+            merkle.push(nh);
+        }
+        merkle.pop().unwrap()
+    }
+
+    pub fn proof_of_work(header: &mut bheader) {
+        loop {
+            let hash = Chain::hash(header);
+            let slice = &hash[..header.difficulty as usize];
+            match slice.parse::<u32>() {
+                Ok(val) => {
+                    if val != 0 {
+                        header.nonce += 1;
+                    } else {
+                        println!("Block hash: {}", hash);
+                        break;
+                    }
+                }
+                Err(_) => {
+                    header.nonce += 1;
+                    continue;
+                }
+            };
+        }
+    }
+
+    pub fn hash<T: serde::Serialize>(item: &T) -> String {
+        let input = serde_json::to_string(&item).unwrap();
+        let mut hasher = Sha256::new();
+        hasher.update(input.as_bytes());
+        let res = hasher.finalize();
+        let vec_res = res.to_vec();
+
+        Chain::hex_to_string(vec_res.as_slice())
+    }
+
+    pub fn hex_to_string(vec_res: &[u8]) -> String {
+        let mut s = String::new();
+        for b in vec_res {
+            write!(&mut s, "{:x}", b).expect("unable to write");
+        }
+        s
+    }
+}
