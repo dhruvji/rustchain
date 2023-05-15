18343d4bf52fad6e4f3a363bea2c3eb7d5ab0380
dhruvji <dhruvgautam@berkeley.edu>
2023-05-14 22:26:35 -0700

    initializing

---
+++ src/main.rs
@@ -0,0 +1,8 @@
+use std::io;
+use std::io::Write;
+use std::process;
+mod rustchain;
+
+fn main() {
+    println!("Hello, world!");
+}
---
+++ src/rustchain.rs
@@ -0,0 +1,5 @@
+use serde_derive::{Deserialize, Serialize};
+use sha2::{Sha256, Digest};
+use std::fmt::Write;
+use chrono::prelude::*;
+
